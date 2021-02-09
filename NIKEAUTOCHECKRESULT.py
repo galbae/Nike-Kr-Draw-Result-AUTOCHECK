5f37bb259ecf952749a5f919b14295d3ce3f612f
galbae <65668215+galbae@users.noreply.github.com>
2021-02-09 16:45:51 +0900

    Update NIKEAUTOCHECKRESULT.py

--- NIKEAUTOCHECKRESULT.py
+++ NIKEAUTOCHECKRESULT.py
@@ -6,16 +6,15 @@ from selenium.webdriver.common.keys import Keys
 import time
 import os, sys
 
-usernamelist = ['1', '2', '3', '4']
-passwordlist = ['1', '2', '3', '4']
-#전체 갯수_xpath /html/body/section/section/article[2]/div/div[2]/div/div[1]/div[1]/div/a[1]/span
+usernamelist = ['1', '2', '3', '4'] #input your usernaem
+passwordlist = ['1', '2', '3', '4'] #input your password
+#ALL_DRAW_COUNT_xpath /html/body/section/section/article[2]/div/div[2]/div/div[1]/div[1]/div/a[1]/span
 options = webdriver.ChromeOptions()
 options.add_argument('User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36')
 options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options) 
-driver.maximize_window()
 
 for i in range(len(usernamelist)):
     driver.delete_all_cookies()
@@ -46,7 +45,7 @@ for i in range(len(usernamelist)):
         else:
             k = k 
             
-        name_list = [] #제품명
+        name_list = [] #ITEM_NAME
         for p in range(1,k+1):
             name_xpath = "/html/body/section/section/article[2]/div/div[2]/div/div[2]/div["+str(p)+"]/div[2]/div[1]/div[2]/span[2]"
             name = driver.find_element_by_xpath(name_xpath)
@@ -55,7 +54,7 @@ for i in range(len(usernamelist)):
 
         time.sleep(1)
 
-        result_list = [] #당첨여부
+        result_list = [] #DRAW_RESULT
         for p in range(1,k+1):
             result_xpath = "/html/body/section/section/article[2]/div/div[2]/div/div[2]/div["+str(p)+"]/div[1]/span[2]"
             result = driver.find_element_by_xpath(result_xpath)
@@ -67,9 +66,9 @@ for i in range(len(usernamelist)):
 
 
     def logout():
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #스크롤
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
 
-        posting = driver.find_element_by_xpath('/html/body/footer/div/div[1]/div/div/p[4]/a') #로그아웃
+        posting = driver.find_element_by_xpath('/html/body/footer/div/div[1]/div/div/p[4]/a')
         posting.click()
         #driver.close()
 
